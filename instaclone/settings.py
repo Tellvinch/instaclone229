065a3fda25073bc3ba61edddbdb1af72ac60da38
Tellvinch imani <tellvinchimani@gmail.com>
2020-07-14 06:27:59 +0300

    add changes

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import os
 import dj_database_url
-import django_heroku
+# import django_heroku
 from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
