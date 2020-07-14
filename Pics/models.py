ab003d87a5f4d05f71afbefea0489be2eeb4869c
Tellvinch imani <tellvinchimani@gmail.com>
2020-07-14 07:22:14 +0300

    add changes

--- Pics/models.py
+++ Pics/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db.models.signals import post_save
 from django.dispatch  import receiver
-# from django.http import Http404
+from django.http import Http404
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
